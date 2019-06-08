970aeb761ab74f795689bd58b39a0b85b178b2b6
Sudhanshu Raheja <sudhanshu@leftshift.io>
2019-06-09 00:07:49 +0700

    [sudhanshu] moved to typescript

--- index.js
+++
@@ -1,8 +0,0 @@
-const path = require('path')
-
-const enWordnet = {
-    "3.0": path.join(__dirname, 'database', '3.0'),
-    "3.1": path.join(__dirname, 'database', '3.1')
-}
-
-module.exports = enWordnet
--- index.test.js
+++
@@ -1,8 +0,0 @@
-const enWordnet = require('./index')
-
-describe('Test if the package is usable', () => {
-    test('Check path for both version exists', () => {
-        expect(enWordnet["3.0"]).toContain("en-wordnet/database/3.0")
-        expect(enWordnet["3.1"]).toContain("en-wordnet/database/3.1")
-    })
-})
