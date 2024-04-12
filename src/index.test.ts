e67e11885ab51f3c6facbf09377e576499b251f1
Sudhanshu Raheja <sudhanshu@leftshift.io>
2024-04-13 03:56:11 +0700

    fixed the test by removing todo

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,9 +1,8 @@
 import { test, expect } from "bun:test";
 import enWordnet from './index'
 
-test.todo("Test if the package is usable", () => {
-  test("Check path for both version exists", () => {
-    expect(enWordnet.get("3.0")).toContain("en-wordnet/database/3.0");
-    expect(enWordnet.get("3.1")).toContain("en-wordnet/database/3.1");
-  });
+test("Check path for both version exists", () => {
+  expect(enWordnet.get("3.0")).toContain("en-wordnet/database/3.0");
+  expect(enWordnet.get("3.1")).toContain("en-wordnet/database/3.1");
 });
+
