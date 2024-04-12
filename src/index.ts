3310360848c27ceb18e7732db5cbacabed61e0bc
Sudhanshu Raheja <sudhanshu@leftshift.io>
2024-04-13 03:20:28 +0700

    migrated from jest to bun test

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
-// import * as path from 'path'
-const path = require("path");
+import * as path from 'path'
 
 const enWordnet = new Map();
 enWordnet.set("3.0", path.join(__dirname, "..", "database", "3.0"));
 enWordnet.set("3.1", path.join(__dirname, "..", "database", "3.1"));
 
-// export default enWordnet;
-module.exports = enWordnet;
+export default enWordnet;
